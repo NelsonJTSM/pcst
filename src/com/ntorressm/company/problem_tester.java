01040b909b3577a6838e4bfea08ba952e3972f15
Nelson Torres <ntorressm@gmail.com>
2018-09-13 20:13:14 -0400

    Got the program working for the first time. Runs the java process, inputs a static String, and compares output to static String

--- src/com/ntorressm/company/problem_tester.java
+++ src/com/ntorressm/company/problem_tester.java
@@ -2,37 +2,52 @@ package com.ntorressm.company;
 
 import java.io.*;
 
+/*
+TODO: Correctly handle execeptions
+TODO: Handle input from file
+TODO: Handle correct output from file
+TODO: Add arguments handling (-d for changing directory e.g. instead of having to pass it)
+ */
+
 public class problem_tester {
 
     static String filename;
 
+    // Solely used for testing
+    static String staticTestInput = "6\nHello\nWorld!\nI am\ncorrect\nNo\nyou're not";
+    static String staticTestOutput = "Hello World!I am correctNo you're not";
+
+    static File processingDirectory;
+    static String processingOutput;
+
+    static File correctInputFile;
+    static File correctOutputFile;
+
+
     /*
     Current correct and only way to call this class
-    java problem_tester dir/to/testing/file/filename.java dir/to/testing/input.in dir/to/testing/output.out
+    java problem_tester test dir/to/testing/ dir/to/testing/input.in dir/to/testing/output.out
      */
 
     public static void main(String[] args) throws Exception {
-        if (args.length == 3) {
-            //System.out.printf("Successful arguments: %s %s %s", args[0], args[1], args[2]);
+        if (args.length == 4) {
+            System.out.printf("Successful arguments: %s %s %s %s\n", args[0], args[1], args[2], args[3]);
 
-            filename = args[0].substring(0, args[0].length()-5);
-            System.out.printf("filename: %s\n", filename);
+            // Process args //
+            filename = args[0];
+            processingDirectory = new File(args[1]);
+            correctInputFile = new File(args[2]);
+            correctOutputFile = new File(args[3]);
 
             // RUN Solution File
-            runProcess("javac " + args[0]);
-            System.out.println("Successfully compiled");
-
-            // Pipe Input into Solution
-            String command2 = "cat " + args[1] + " | java " + filename;
-            System.out.println("Trying command: " + command2);
-            OutputStream is = runProcess(command2);
-
-            // Save output
-            System.out.println("InputStream is\n" + getOutput(is));
+            runSolution();
 
             // Compare solution output to correct output
-
-            // Print out true or false whether it is right or wrong
+            if (processingOutput.equals(staticTestOutput)) {
+                System.out.println("Correct");
+            } else {
+                System.out.println("Incorrect");
+            }
         } else {
             System.out.println("ERROR: Incorrect format");
             System.out.println("Use this format: java problem_tester dir/to/testing/file/filename.java dir/to/testing/input.in dir/to/testing/output.out");
@@ -40,13 +55,64 @@ public class problem_tester {
         }
     }
 
-    private static OutputStream runProcess(String command) throws Exception {
-        Process process = Runtime.getRuntime().exec(command);
-        // Handle error
-        return process.getOutputStream();
+    public static void runSolution() throws Exception {
+        System.out.println("Running solution");
+
+        ProcessBuilder processingBuilder = new ProcessBuilder("java", filename);
+        processingBuilder.directory(processingDirectory);
+        processingBuilder.redirectErrorStream(true);
+        Process process = processingBuilder.start();
+
+        OutputStream stdin = process.getOutputStream();
+        InputStream stdout = process.getInputStream();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(stdout));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(stdin));
+        System.out.println("pid: " + process.pid());;
+
+
+        // Test input into the process
+        writeInput(writer);
+
+        // Read output
+        readOutput(reader);
+
+        // Finished with BufferedReader/Writer
+        reader.close();
     }
 
-    private static String getOutput(OutputStream is) throws IOException {
-        return null;
+    /*
+    For now saves the output of the processed program into a String
+    TODO: Save output in a different way (to more easily compare
+     */
+    public static void readOutput(BufferedReader reader) throws Exception {
+        System.out.println("Reading output...");
+
+        try {
+            processingOutput = "";
+            String lastLine; // Assumes there is a first line to begin with
+
+            while ((lastLine = reader.readLine()) != null) {
+                processingOutput += lastLine;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Finished reading output!");
+    }
+
+    /*
+    TODO: Write this function correctly, for now it passes a static String in this class
+     */
+    public static void writeInput(BufferedWriter writer) throws Exception {
+        System.out.println("Writing output...");
+
+        writer.write(staticTestInput);
+
+        writer.flush();
+        writer.close();
+
+        System.out.println("Finished writing output!");
     }
 }

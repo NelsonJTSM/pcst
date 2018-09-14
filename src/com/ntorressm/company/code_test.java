9ef76b59899adade7b805c811c3241c2c24763a0
Nelson Torres <ntorressm@gmail.com>
2018-09-14 17:51:38 -0400

    Removed code_test and problem_tester

--- src/com/ntorressm/company/code_test.java
+++
@@ -1,7 +0,0 @@
-package com.ntorressm.company;
-
-public class code_test {
-    public static void main(String[] args) {
-        System.out.printf("Current directiory: %s\n", System.getProperty("user.dir"));
-    }
-}
